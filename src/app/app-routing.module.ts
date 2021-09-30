f0c4175e853d60e55c19be79f8d3bbae531d7c59
Jufra <Jufra>
2021-09-30 12:15:40 +0200

    creating main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'movies', loadChildren: () => import('./pages/movies/movies.module').then(m => m.MoviesModule) }, 
   { path: 'actors', loadChildren: () => import('./pages/actors/actors.module').then(m => m.ActorsModule) }, 
-  { path: 'companies', loadChildren: () => import('./pages/companies/companies.module').then(m => m.CompaniesModule) }
+  { path: 'companies', loadChildren: () => import('./pages/companies/companies.module').then(m => m.CompaniesModule) },
+  { path: '', loadChildren: () => import('./main/main.module').then(m => m.MainModule) }
 ];
 
 @NgModule({
