a8195c8107b0df447f37f4f70644be21df828a95
Jufra <Jufra>
2021-09-30 12:09:46 +0200

    organizing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'movies', loadChildren: () => import('./pages/movies/movies.module').then(m => m.MoviesModule) }, 
+  { path: 'actors', loadChildren: () => import('./pages/actors/actors.module').then(m => m.ActorsModule) }, 
+  { path: 'companies', loadChildren: () => import('./pages/companies/companies.module').then(m => m.CompaniesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
